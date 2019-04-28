fb72a2fac88c337861dde140fbfe298e559881b6
Adam S <leavinit@gmail.com>
2019-04-28 15:54:47 -0500

    Added js to display the character images

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ class player {
         this.charAP = stats.AP;
         this.charHP = stats.HP;
         this.charCAP = stats.CAP;
-
+        this.display();
     }
     
     // Character's Name
@@ -19,6 +19,16 @@ class player {
     charHP = "";
     charCAP= "";
 
+    // Character Display Function (Shows images in characterList Div for player to select)
+    display = function(){
+        //Dyanmically add images
+        
+        $("#characterList")
+        .append($('<img>',{id:this.charName,src:this.charImage,alt:this.charName}));
+    
+        $("#"+this.charName).wrap($('<div>',{class:"character"}));
+        
+    };
 }
 
 //Event Listeners
@@ -26,9 +36,28 @@ class player {
 
 
 //Testing
-player1  = new player(
-    "Boba Fett",
-    "assets/images/boba-fett.webp",
+boba  = new player(
+    "Boba_Fett",
+    "assets/images/boba_fett.jpg",
     {AP:10,HP:100,CAP:30}
 );
-console.log(player1.charCAP);
+jabba  = new player(
+    "Jabba_the_Hutt",
+    "assets/images/jabba-the-hutt.jpeg",
+    {AP:10,HP:100,CAP:30}
+);
+lando  = new player(
+    "Lando_Calrissian",
+    "assets/images/lando.jpeg",
+    {AP:10,HP:100,CAP:30}
+);
+yoda  = new player(
+    "Yoda",
+    "assets/images/yoda.jpg",
+    {AP:10,HP:100,CAP:30}
+);
+
+
+// console.log(player1.charCAP);
+// $('body').text(player1.charCAP);
+// $('#characterList').prepend($('<img>',{id:player1.charName,src:player1.charImage}));
